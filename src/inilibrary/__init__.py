77b1211c8d99e88a7203ae3a5f7b35a7a4eec025
Shrinivas Bagewadi <shrinivasbagewadi@gmail.com>
2025-01-02 12:33:18 +0530

    Update __init__.py

--- src/inilibrary/__init__.py
+++ src/inilibrary/__init__.py
@@ -1,6 +1,6 @@
 from .INILibrary import INILibrary
 from robot.api.deco import library
 
-@library(scope="GLOBAL", version="1.0.2")
+@library(scope="GLOBAL", version="1.0.4")
 class INILibrary(INILibrary):
     pass
